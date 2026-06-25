e2ad1dd90dac174332ddc89ff7adb15e610837d8
JamesMangao <bienvenido.publico@cdsp.edu.ph>
2026-06-25 15:40:57 +0800

    fix: add missing Request import in AdminIntentionController

--- app/Http/Controllers/AdminIntentionController.php
+++ app/Http/Controllers/AdminIntentionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\MassIntention;
 use App\Services\LogService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
 use App\Http\Requests\StoreIntentionRequest;
 
