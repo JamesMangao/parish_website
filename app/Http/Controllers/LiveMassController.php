361be0039c1a9cdcfe88409d41e0f81ee8001414
James Mangao <144905900+JamesMangao@users.noreply.github.com>
2026-08-23 10:16:35 +0800

    Update LiveMassController.php

--- app/Http/Controllers/LiveMassController.php
+++ app/Http/Controllers/LiveMassController.php
@@ -10,10 +10,30 @@ class LiveMassController extends Controller
     {
         $request->validate(['state' => 'required|in:on,off']);
 
-        $request->state === 'on'
-            ? Cache::put('manual_live_override', true, now()->addHours(3)) // auto-expires
-            : Cache::forget('manual_live_override');
+        if ($request->state === 'on') {
+            Cache::put('manual_live_override', true, now()->addHours(3));
+        } else {
+            Cache::forget('manual_live_override');
+            Cache::forget(FacebookLiveWebhookController::CACHE_KEY);
+        }
 
         return back()->with('success', 'Live Mass override updated.');
+    }
+
+    public function updateFacebookLink(Request $request)
+    {
+        $request->validate([
+            'facebook_live_url' => ['nullable', 'url', 'regex:/^https?:\/\/(www\.)?(facebook\.com|fb\.watch|m\.facebook\.com)\//i'],
+        ]);
+
+        $url = $request->input('facebook_live_url');
+
+        if ($url) {
+            Cache::put(FacebookLiveWebhookController::CACHE_KEY, $url, now()->addHours(4));
+            return back()->with('success', 'Facebook Live link updated.');
+        }
+
+        Cache::forget(FacebookLiveWebhookController::CACHE_KEY);
+        return back()->with('success', 'Facebook Live link cleared.');
     }
 }
